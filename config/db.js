7488dbb46f84068b856ed087c389bd84b8cc101b
airtonbjunior <airtonbjunior@gmail.com>
2024-11-05 10:36:52 -0300

    dados sensiveis enviados para o .env

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 // Configuração do Banco de Dados
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 
-const sequelize = new Sequelize('ponto', 'postgres', 'ceub123456', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 })
 
